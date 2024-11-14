055884bcb361bdc51726aadd9d76ed0bd13edfca
jwli23 <johnli2001@gmail.com>
2024-11-14 12:03:31 -0600

    Add LineGraph prototype

--- blood-test-results/src/App.js
+++ blood-test-results/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import TestHistoryGraph from './components/TestHistoryGraph';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Button variant='Contained'>Submit!</Button>
+        <TestHistoryGraph testValues={[1,2,3]} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ blood-test-results/src/components/TestHistoryGraph.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+import { linkClasses } from '@mui/material';
+const arrayRange = (stop) => {
+  return Array.from(
+  { length: stop },
+  (value, index) => 1 + index
+  );
+}
+
+const TestHistoryGraph = ({ testValues }) => {
+    return (
+      <LineChart 
+        xAxis = {[{ data: arrayRange(testValues.length)}]}
+        series = {[{ data: testValues}]}
+        width = {500}
+        height={300}
+      />
+    )
+}
+
+export default TestHistoryGraph
